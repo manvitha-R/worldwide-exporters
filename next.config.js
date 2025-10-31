4e92b82c5ee7eb4c49cfe1852daaa0f7cecaaf80
manvitha-R <manvitharajeev2908@gmail.com>
2025-10-31 15:35:08 +0530

    fix: replace next.config.ts with next.config.js for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
+  experimental: {
+    appDir: true,
+  },
+ eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
